0a1c5e9255605eee30a5a054ee3c01df9ff58a06
Steven Dall <stvndall@gmail.com>
2016-04-17 13:47:04 +0200

    test for spinner

--- src/main/scala/game/GameOfLifeController.scala
+++ src/main/scala/game/GameOfLifeController.scala
@@ -3,16 +3,16 @@ package game
 object GameOfLifeController {
 
 
-  def GetStayingPoints(points: Array[PointInWorld]): Array[PointInWorld] = {
-    Array()
+  def GetStayingPoints(lc: LifeCheckers,points: Array[PointInWorld]): Array[PointInWorld] = points.length match{
+    case 0 => Array()
+    case _ => (lc.ShouldStayAlive(points.head) +: GetStayingPoints(lc, points.tail)).filter(cell => cell != null)
   }
 
-  def GetNewLifePoints(points: Array[PointInWorld]): Array[PointInWorld] = {
-    Array()
+  def GetNewLifePoints(lc: LifeCheckers, points: Array[PointInWorld]): Array[PointInWorld] = {
+    lc.PointsThatShouldLightUp().filter(cell => cell != null)
   }
 
-  def apply(points: Array[PointInWorld]): Array[PointInWorld] = points.length match {
-    case 0 => points
-    case _ => GetStayingPoints(points) ++ GetNewLifePoints(points)
+  def apply(points: Array[PointInWorld]): Array[PointInWorld] = {
+    GetStayingPoints(new LifeCheckers(points),points) ++ GetNewLifePoints(new LifeCheckers(points), points)
   }
 }
--- src/test/scala/game/GameOfLifeControllerTests.scala
+++ src/test/scala/game/GameOfLifeControllerTests.scala
@@ -5,7 +5,7 @@ import org.scalatest.FunSuite
 class GameOfLifeControllerTests extends FunSuite {
 
   test("Empty world should return empty") {
-    assert(GameOfLifeController(new Array[PointInWorld](0)).length == 0)
+    assert(GameOfLifeController(Array()).length == 0)
   }
 
   test("a cell with no neighbours should die") {
@@ -20,7 +20,11 @@ class GameOfLifeControllerTests extends FunSuite {
     assert(PointInWorld(1, 1).Neighbours().distinct.length == 8)
   }
 
-  test("spinner should rotate"){
-    assert(GameOfLifeController(Array(new PointInWorld(1, 1), new PointInWorld(1,0), new PointInWorld(1,-1)))== Array(new PointInWorld(1, 1), new PointInWorld(0,1), new PointInWorld(-1,1)))
+  test("spinner should rotate") {
+    val arrayOfPoints: Array[PointInWorld] = GameOfLifeController(Array(new PointInWorld(1, 1), new PointInWorld(1, 0), new PointInWorld(1, -1)))
+    assert(arrayOfPoints.length == 3)
+    assert(arrayOfPoints.contains(new PointInWorld(0, 0)))
+    assert(arrayOfPoints.contains(new PointInWorld(1, 0)))
+    assert(arrayOfPoints.contains(new PointInWorld(2, 0)))
   }
 }
